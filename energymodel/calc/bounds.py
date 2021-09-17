2161d265a6c4ebc64c4a9e6ca9cd5e5fd162b2b3
bsnacks000 <bsnacks000@gmail.com>
2021-09-17 11:56:46 -0400

    changed interface for bounds

--- energymodel/calc/bounds.py
+++ energymodel/calc/bounds.py
@@ -3,9 +3,9 @@
 Tuple return types correspond directly to coeffs in the method signature of `_lib.models`. 
 """
 
-from typing import Tuple
+from typing import Tuple, Union
 import numpy as np 
-from ..nptypes import OneDimNDArray
+from ..nptypes import OneDimNDArray, NByOneNDArray
 
 BoundTuple = Tuple[Tuple[float, ...], Tuple[float, ...]]
 
@@ -15,29 +15,33 @@ FourParameterBoundary = Tuple[float, float, float, float]
 FiveParameterBoundary = Tuple[float, float, float, float ,float]
 
 
-def twop(*args) -> Tuple[TwoParameterBoundary, TwoParameterBoundary]: 
+def twop(*args, **kwargs) -> Tuple[TwoParameterBoundary, TwoParameterBoundary]: # << XXX need this to satisfy the interface even though we pass a constant 
     return ((0, -np.inf),(np.inf, np.inf))
 
 
-def threepc(X: OneDimNDArray) -> Tuple[ThreeParameterBoundary, ThreeParameterBoundary]:
+def threepc(X: Union[OneDimNDArray,NByOneNDArray]) -> Tuple[ThreeParameterBoundary, ThreeParameterBoundary]:
+    X = X.squeeze()
     min_cp = X[int(len(X)/4)]
     max_cp = X[int(3 * len(X)/4)]
     return ((0,0,min_cp), (np.inf,np.inf, max_cp))
 
 
-def threeph(X: OneDimNDArray) -> Tuple[ThreeParameterBoundary, ThreeParameterBoundary]:
+def threeph(X: Union[OneDimNDArray,NByOneNDArray]) -> Tuple[ThreeParameterBoundary, ThreeParameterBoundary]:
+    X = X.squeeze()
     min_cp = X[int(len(X)/4)]
     max_cp = X[int(3 * len(X)/4)]
     return ((0,-np.inf, min_cp), (np.inf,0, max_cp))
 
 
-def fourp(X: OneDimNDArray) -> Tuple[FourParameterBoundary, FourParameterBoundary]:
+def fourp(X: Union[OneDimNDArray,NByOneNDArray]) -> Tuple[FourParameterBoundary, FourParameterBoundary]:
+    X = X.squeeze()
     min_cp = X[int(len(X)/4)]
     max_cp = X[int(3 * len(X)/4)]
     return ((0,-np.inf, -np.inf, min_cp), (np.inf,np.inf, np.inf, max_cp))
 
 
-def fivep(X: OneDimNDArray) -> Tuple[FiveParameterBoundary, FiveParameterBoundary]:
+def fivep(X: Union[OneDimNDArray,NByOneNDArray]) -> Tuple[FiveParameterBoundary, FiveParameterBoundary]:
+    X = X.squeeze()
     min_cp1 = X[int((2/8) * len(X))]
     max_cp1 = X[int((3/8) * len(X))]
     min_cp2 = X[int((5/8) * len(X))]
