e1e5eef96544b65800ebcd6486a9d4a9459ee2b8
bsnacks000 <bsnacks000@gmail.com>
2021-11-18 08:26:46 -0500

    added modify schemas

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -1,8 +1,13 @@
+from typing import List
 from energymodel import schemas 
 
 import numpy as np
 import pytest
 import pydantic
+import dataclasses
+
+from energymodel.nptypes import NByOneNDArrayField, OneDimNDArrayField, AnyByAnyNDArrayField
+
 
 def test_curvefitestimatordatamodel_handles_1d_xdata(): 
 
@@ -171,3 +176,44 @@ def test_adjustedenergychangepointmodelsavingsresult_with_non_required_data(
     }
 
     schemas.SavingsResult(**data)
+
+
+def test_openapi_schemas_are_correctly_generated_for_custom_nptypes(): 
+
+    @dataclasses.dataclass 
+    class Check:
+        a: OneDimNDArrayField 
+        b: NByOneNDArrayField
+        c: AnyByAnyNDArrayField  
+    
+
+    class CheckModel(pydantic.BaseModel): 
+        check : Check
+        thing : int 
+        mylist : List[float]
+        
+        class Config:
+            json_encoders = {
+                np.ndarray : lambda v: v.tolist()
+            }
+
+    check = Check(
+        a=[1,2,3], 
+        b=[[1,],[2,],[3,],], 
+        c=[[1,2,3],[4,5,6]], 
+        )
+    CheckModel(check=check, thing=42, mylist=[7,8,9])
+    schema = CheckModel.schema()
+
+
+    assert 'a' in schema['definitions']['Check']['properties']
+    assert "b" in schema['definitions']['Check']['properties']
+    assert "c" in schema['definitions']['Check']['properties']
+
+    a = {"title": "A","type": "array","items": {"type": "number"}}
+    b = {"title": "B","type": "array","items": {"type": "array","items": {"type": "number"}}}
+    c = {"title": "C","type": "array","items": {"type": "array","items": {"type": "number"}}}
+
+    assert schema['definitions']['Check']['properties']['a'] == a 
+    assert schema['definitions']['Check']['properties']['b'] == b 
+    assert schema['definitions']['Check']['properties']['c'] == c 
