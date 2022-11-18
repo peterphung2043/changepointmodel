ea53ffa7593cd613656151535792c96f2417af16
bsnacks000 <bsnacks000@gmail.com>
2022-11-18 10:01:18 -0500

    fix test result

--- tests/test_lib_savings.py
+++ tests/test_lib_savings.py
@@ -6,10 +6,10 @@ from numpy.testing import assert_almost_equal
 
 def test_adjusted_returns_correct_value(): 
 
-    res = adjusted(42,43,0.5,2, 12, 12, 0.8)
+    res = adjusted(42, 43, 0.5, 2, 12, 12, 0.8)
     # XXX if pre and post are identical we get div by zero error and nans
     # (-1, -0.08333333333333333, -44, 11.321001530179318)
-    assert_almost_equal(res, (-1, -0.0833, -44, 11.321), decimal=2)
+    assert_almost_equal(res, (-1, -0.0833, -0.02, 11.321), decimal=2)
 
 def test_weather_normalized_returns_correct_value(): 
 
