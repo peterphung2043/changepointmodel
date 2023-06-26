f8a093760d06c13f163de77fcec9b077e436b6ae
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2023-06-26 13:07:33 -0400

    Add twopc code, return types, and tests

--- changepointmodel/core/calc/dpop.py
+++ changepointmodel/core/calc/dpop.py
@@ -3,26 +3,28 @@ import numpy.typing as npt
 from typing import Tuple, List, Optional
 from changepointmodel.core.nptypes import OneDimNDArrayField
 
-
-def twop():
-    ...
+HeatingCoolingPoints = Tuple[float, float]
 
 
-def threepc(X: OneDimNDArrayField, changepoint: float) -> float:
-    return sum(X >= changepoint)
+def twop(X: OneDimNDArrayField, slope: float) -> HeatingCoolingPoints:
+    return (len(X), 0) if slope <= 0 else (0, len(X))
 
 
-def threeph(X: OneDimNDArrayField, changepoint: float) -> float:
-    return sum(X <= changepoint)
+def threepc(X: OneDimNDArrayField, changepoint: float) -> HeatingCoolingPoints:
+    return 0, sum(X >= changepoint)
 
 
-def fourp(X: OneDimNDArrayField, changepoint: float) -> Tuple[float, float]:
+def threeph(X: OneDimNDArrayField, changepoint: float) -> HeatingCoolingPoints:
+    return sum(X <= changepoint), 0
+
+
+def fourp(X: OneDimNDArrayField, changepoint: float) -> HeatingCoolingPoints:
     heatnum = sum(X <= changepoint)
     coolnum = sum(X > changepoint)
     return coolnum, heatnum
 
 
-def fivep(X: OneDimNDArrayField, changepoints: List[float]) -> Tuple[float, float]:
+def fivep(X: OneDimNDArrayField, changepoints: List[float]) -> HeatingCoolingPoints:
     heatnum = sum(X <= changepoints[0])
     coolnum = sum(X >= changepoints[1])
     return coolnum, heatnum
--- tests/test_lib_dpop.py
+++ tests/test_lib_dpop.py
@@ -11,18 +11,21 @@ def test_dpop_good(dummy_result_for_filter_extra_good_for_dpop_test):
     fourp_data = data[3]
     fivep_data = data[4]
 
-    # 2P testing goes here
-    ...
+    result = dpop.twop(twop_data.result.input_data.X, -1)
+    assert result == (len(twop_data.result.input_data.X), 0)
+
+    result = dpop.twop(twop_data.result.input_data.X, 1)
+    assert result == (0, len(twop_data.result.input_data.X))
 
     result = dpop.threepc(
         threepc_data.result.input_data.X, threepc_data.result.coeffs.changepoints[0]
     )
-    assert result == 8
+    assert result == (0, 8)
 
     result = dpop.threeph(
         threeph_data.result.input_data.X, threeph_data.result.coeffs.changepoints[0]
     )
-    assert result == 5
+    assert result == (5, 0)
 
     result = dpop.fourp(
         fourp_data.result.input_data.X, fourp_data.result.coeffs.changepoints[0]
@@ -38,24 +41,20 @@ def test_dpop_good(dummy_result_for_filter_extra_good_for_dpop_test):
 def test_dpop_bad(dummy_result_for_filter_extra_bad_for_dpop_test):
     data = dummy_result_for_filter_extra_bad_for_dpop_test
 
-    twop_data = data[0]
     threepc_data = data[1]
     threeph_data = data[2]
     fourp_data = data[3]
     fivep_data = data[4]
 
-    # 2P testing goes here
-    ...
-
     result = dpop.threepc(
         threepc_data.result.input_data.X, threepc_data.result.coeffs.changepoints[0]
     )
-    assert result == 11
+    assert result == (0, 11)
 
     result = dpop.threeph(
         threeph_data.result.input_data.X, threeph_data.result.coeffs.changepoints[0]
     )
-    assert result == 2
+    assert result == (2, 0)
 
     result = dpop.fourp(
         fourp_data.result.input_data.X, fourp_data.result.coeffs.changepoints[0]
